8ed57590ceb41b4780434b4b0506fecc44f4ab33
Wojciech Kluz <urban1996n@gmail.com>
2026-03-07 08:55:31 +0100

    (chore) loosen up ts checks

--- src/Domain/Identity/Auth/AuthContext.tsx
+++ src/Domain/Identity/Auth/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useContext, useEffect, ReactNode } from 'react';
+import { createContext, useState, useContext, useEffect, ReactNode } from 'react';
 
 interface AuthContextType {
   user: string | null;
